4e11eac27b4c20ded678abc421dd27b0e0de6cd0
andreasmmadjiah <andreasmmadjiah@gmail.com>
2021-09-28 23:59:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def predict():
          <title>Prediction Result! EYEYEYEYEY</title>
       </head>
       <body>
-         <h3>Prediction Coba 23:50</h3>
+         <h3>Prediction Coba 24</h3>
          <div>This is the predicted classification oy <strong>{{ response.prediction }}</strong>.</div>
          <div>with data {{response.data}}</div>
       </body>
